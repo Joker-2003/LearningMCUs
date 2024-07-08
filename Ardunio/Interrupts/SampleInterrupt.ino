1fb82e7796e7714d0fe0d2d2f5fd6b8d8cf60f0f
Joker <srivartkovid@gmail.com>
2024-07-08 16:41:24 +0530

    feat: added interrputs

---
+++ Ardunio/Interrupts/SampleInterrupt.ino
@@ -0,0 +1,12 @@
+const int interruptPin = 2; // External interrupt pin
+
+void setup() {
+  pinMode(interruptPin, INPUT_PULLUP); // Initialize pin as input with internal pull-up resistor
+  attachInterrupt(digitalPinToInterrupt(interruptPin), ISR, CHANGE); // Attach ISR to pin 2
+  Serial.begin(9600); // Initialize serial communication
+}
+
+void loop() {
+  // Main program loop
+}
+
