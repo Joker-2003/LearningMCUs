681d5b0eb24051dc0151c3b51ec175eb026d0b7e
Joker <srivartkovid@gmail.com>
2024-08-09 03:32:52 +0530

    ARDUINO: added relay sketch

---
+++ Ardunio/Relays/Relay.ino
@@ -0,0 +1,13 @@
+#define RELAY_PIN 7
+void setup()
+{
+	// initialize digital pin RELAY_PIN as an output.
+	pinMode(RELAY_PIN, OUTPUT);
+}
+void loop()
+{
+	digitalWrite(RELAY_PIN, HIGH); // turn the RELAY on
+	delay(1000);				   // wait for a second
+	digitalWrite(RELAY_PIN, LOW);  // turn the RELAY off
+	delay(1000);				   // wait for a second
+}
